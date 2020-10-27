78783ffc009e704ca55a35e74385b36c7df96929
AlionaMu <34584155+AlionaMu@users.noreply.github.com>
2020-10-27 23:32:55 +0300

    feat: add solutions

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+module.exports =  class DepthCalculator {
+  calculateDepth(arr) {
+    let depth = 1;
+    
+    for (let i = 0; i < arr.length; i++) {
+      let add = 1
+        if (Array.isArray(arr[i])) {
+          add += this.calculateDepth(arr[i]);
+          if (add > depth) {
+            depth = add
+          }
+        }
+    }
+    return depth
   }
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,85 @@
 const CustomError = require("../extensions/custom-error");
 
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mod) { 
+      this.mod = mod;
+  }
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error
+    }
+
+    let diff = 0;
+    let res = [];
+    let b = [];
+
+    let mArr = message.toUpperCase().split('');
+    let kArr = key.toUpperCase().split('');
+
+    for (let i = 0; i < mArr.length; i++) {
+      if (mArr[i].match(/[a-z]/gi)) {
+        if (diff >= kArr.length) 
+          diff = 0;  
+          b.push(kArr[diff])
+          diff++
+        }
+        else { b.push('-')}
+    }
+
+    for (let i = 0; i < mArr.length; i++) {
+      if (mArr[i].match(/[a-z]/gi)) {
+        let letter = mArr[i].charCodeAt() + b[i].charCodeAt() - 65;
+        res.push(String.fromCharCode(letter > 64 && letter < 91 ? letter : letter - 26))
+      }
+      else {
+        res.push(mArr[i])
+      }
+    }
+    if (this.mod == false) {
+      return res.reverse().join('')
+     } else {
+       return res.join('')
+     }
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error
+    }
+
+    let diff = 0;
+    let res = [];
+    let b = [];
+
+    let mArr = message.toUpperCase().split('');
+    let kArr = key.toUpperCase().split('');
+
+    for (let i = 0; i < mArr.length; i++) {
+      if (mArr[i].match(/[a-z]/gi)) {
+        if (diff >= kArr.length) 
+          diff = 0;  
+          b.push(kArr[diff])
+          diff++
+        }
+        else { b.push('-')}
+    }
+
+    for (let i = 0; i < mArr.length; i++) {
+      if (mArr[i].match(/[a-z]/gi)) {
+        let letter = mArr[i].charCodeAt() - b[i].charCodeAt() + 65;
+        res.push(String.fromCharCode(letter > 64 && letter < 91 ? letter : letter + 26))
+      }
+      else {
+        res.push(mArr[i])
+      }
+    }
+
+    if (this.mod == false) {
+     return res.reverse().join('')
+    } else {
+      return res.join('')
+    }
   }
 }
-
 module.exports = VigenereCipheringMachine;
